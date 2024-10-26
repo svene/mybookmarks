acfa31d0ad6f3ebb10891d4a5d7b09ccb5e1f417
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-10-26 05:16:50 +0200

    moved PUT handler to new PutBookmarkAction. idea of an action: HTTP handler which returns HTML, mostly oob HTML PutBookmarkAction is the PoC. atm it is implemented as a component but could be implemented differently

---
+++ src/main/java/org/svenehrke/mybookmarks/actions/putbookmark/PutBookmarkAction.java
@@ -0,0 +1,43 @@
+package org.svenehrke.mybookmarks.actions.putbookmark;
+
+import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.svenehrke.mybookmarks.components.card.CardComponent;
+import org.svenehrke.mybookmarks.components.existingtags.ExistingTagsComponent;
+import org.svenehrke.mybookmarks.service.BookmarkSessionService;
+
+import java.math.BigInteger;
+
+@ViewComponent
+@Controller
+@AllArgsConstructor
+@Slf4j
+public class PutBookmarkAction {
+
+	public static final String URL = "/edit/inline/putbookmark";
+
+	private final BookmarkSessionService bookmarkSessionService;
+	public final CardComponent cardComponent;
+	public final ExistingTagsComponent existingTagsComponent;
+
+	public record Ctx(PutBookmarkAction ME, BigInteger id) implements ViewContext {}
+
+	public Ctx ctx(BigInteger id) {
+		return new Ctx(this, id);
+	}
+
+	@PutMapping(URL)
+	public Ctx putBookmark(
+		@RequestParam BigInteger id,
+		@RequestParam String url,
+		@RequestParam String tags
+	) {
+		bookmarkSessionService.putBookmark(id, url, tags);
+		return ctx(id);
+	}
+}

5d3cc15eae06c63cb4578365241a093f6690c0ce
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-11-05 19:11:40 +0100

    refactoring of CsvTextComponent

--- src/main/java/org/svenehrke/mybookmarks/components/csvtext/CsvTextComponent.java
+++ src/main/java/org/svenehrke/mybookmarks/components/csvtext/CsvTextComponent.java
@@ -4,33 +4,28 @@ import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
 import de.tschuehly.spring.viewcomponent.jte.ViewContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.svenehrke.mybookmarks.model.Bookmark;
-import org.svenehrke.mybookmarks.service.BookmarkService;
 import org.svenehrke.mybookmarks.service.BookmarkSessionService;
 import org.svenehrke.mybookmarks.service.BookmarkUtil;
-
-import java.util.List;
 
 @ViewComponent
 @RequiredArgsConstructor
 @Controller
 public class CsvTextComponent {
 
-	public final BookmarkService bookmarkService;
 	public final BookmarkSessionService bookmarkSessionService;
 
-	public record Ctx(CsvTextComponent ME) implements ViewContext {}
+	public record Ctx(CsvTextComponent ME) implements ViewContext {
+		public String convertBookmarksToCSV() {
+			StringBuilder sb = new StringBuilder();
+			ME.bookmarkSessionService.getCsvParseResult().bookmarks().forEach(it -> {
+				sb.append(it.url() + ";" + BookmarkUtil.toTagsString(it.tags()) + System.lineSeparator());
+			});
+			return sb.toString();
+		}
+	}
 
 	public Ctx ctx() {
 		return new Ctx(this);
-	}
-
-	public String convertBookmarksToCSV(List<Bookmark> bookmarks) {
-		StringBuilder sb = new StringBuilder();
-		bookmarks.forEach(it -> {
-			sb.append(it.url() + ";" + BookmarkUtil.toTagsString(it.tags()) + System.lineSeparator());
-		});
-		return sb.toString();
 	}
 
 }
